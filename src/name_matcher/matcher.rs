32ec7eeccfd90251ae9f99c84bc96e3c581b98ea
Daniel Gilchrist <danniieelg@gmail.com>
2022-12-26 02:14:21 +1000

    Extract common name matcher code (#13)

--- src/name_matcher/matcher.rs
+++ src/name_matcher/matcher.rs
@@ -1,11 +1,20 @@
-use crate::name_matcher::{
-    move_names::MOVE_NAMES, pokemon_names::POKEMON_NAMES, type_names::TYPE_NAMES,
+use crate::{
+    formatter::capitalise,
+    name_matcher::{move_names::MOVE_NAMES, pokemon_names::POKEMON_NAMES, type_names::TYPE_NAMES},
 };
+
+use std::process::exit;
 
 use ngrammatic::{Corpus, CorpusBuilder, Pad};
 use once_cell::sync::Lazy;
 
 static MIN_SIMILARITY: f32 = 0.4;
+
+pub enum MatcherType {
+    Pokemon,
+    Move,
+    Type,
+}
 
 pub struct NameMatcher {
     names: Vec<String>,
@@ -37,14 +46,38 @@ impl NameMatcher {
     }
 }
 
-pub fn pokemon_matcher() -> NameMatcher {
+pub fn try_suggest_name(name: &str, matcher_type: MatcherType) -> ! {
+    let (name_matcher, keyword) = matcher_and_keyword(matcher_type);
+
+    match name_matcher.find_match(name) {
+        Some(similar_name) => {
+            println!("Unknown {} \"{}\"", keyword, name);
+            println!("Did you mean \"{}\"?", similar_name);
+            exit(1);
+        }
+        None => {
+            println!("{} \"{}\" doesn't exist", capitalise(&keyword), name);
+            exit(1);
+        }
+    }
+}
+
+fn matcher_and_keyword(matcher_type: MatcherType) -> (NameMatcher, String) {
+    match matcher_type {
+        MatcherType::Move => (move_matcher(), String::from("move")),
+        MatcherType::Pokemon => (pokemon_matcher(), String::from("pokemon")),
+        MatcherType::Type => (type_matcher(), String::from("type")),
+    }
+}
+
+fn pokemon_matcher() -> NameMatcher {
     NameMatcher::new(Lazy::force(&POKEMON_NAMES).to_owned())
 }
 
-pub fn type_matcher() -> NameMatcher {
+fn type_matcher() -> NameMatcher {
     NameMatcher::new(Lazy::force(&TYPE_NAMES).to_owned())
 }
 
-pub fn move_matcher() -> NameMatcher {
+fn move_matcher() -> NameMatcher {
     NameMatcher::new(Lazy::force(&MOVE_NAMES).to_owned())
 }
