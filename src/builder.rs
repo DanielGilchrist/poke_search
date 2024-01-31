fd05afe9412e6630cdb8c8a6edac9ebb1c0953e4
Daniel Gilchrist <danniieelg@gmail.com>
2024-01-31 00:14:55 +0000

    Remove append_builder and instead implement Appendable for Builder

--- src/builder.rs
+++ src/builder.rs
@@ -13,10 +13,6 @@ impl Builder {
         self.0.push_str(str.to_appendable());
     }
 
-    pub fn append_builder(&mut self, builder: Self) {
-        self.append(builder.to_string());
-    }
-
     pub fn append_c(&mut self, c: char) {
         self.0.push(c);
     }
@@ -42,10 +38,16 @@ pub trait Appendable {
     fn to_appendable(&self) -> &str;
 }
 
+impl Appendable for Builder {
+  fn to_appendable(&self) -> &str {
+    &self.0
+  }
+}
+
 impl Appendable for String {
-    fn to_appendable(&self) -> &str {
-        self.as_str()
-    }
+  fn to_appendable(&self) -> &str {
+    self
+  }
 }
 
 impl Appendable for &str {
