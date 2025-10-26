525204ad9b87cc7fc3a96fad0db2bcccd56f55a4
Daniel Gilchrist <daniel@workforce.com>
2025-10-26 01:45:54 +0000

    Fix tests after last commit

--- tests/pokemon_command_test.rs
+++ tests/pokemon_command_test.rs
@@ -74,24 +74,10 @@ Stats
 
 Abilities
   Name: Static
-  Description: Whenever a move makes contact with this Pokémon, the move's user has a 30%
-    chance of being paralyzed.
-
-    Pokémon that are immune to electric-type moves can still be paralyzed by this
-    ability.
-
-    Overworld: If the lead Pokémon has this ability, there is a 50% chance that
-    encounters will be with an electric Pokémon, if applicable.
+  Description: Has a 30% chance of paralyzing attacking Pokémon on contact.
 
   Name: Static
-  Description: Whenever a move makes contact with this Pokémon, the move's user has a 30%
-    chance of being paralyzed.
-
-    Pokémon that are immune to electric-type moves can still be paralyzed by this
-    ability.
-
-    Overworld: If the lead Pokémon has this ability, there is a 50% chance that
-    encounters will be with an electric Pokémon, if applicable."
+  Description: Has a 30% chance of paralyzing attacking Pokémon on contact."
     );
 
     let actual = run(&mock_client, cli).await.to_string();
